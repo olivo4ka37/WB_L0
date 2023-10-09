4560fc8cec5237d33a1b843013c90995de0cbd71
Egor Kostylev <46248025+olivo4ka37@users.noreply.github.com>
2023-10-09 04:54:19 +0300

    Created directory repository, and file postgres.go inside this directory. In postgres.go was implemented type Config that we need to connection to our database, also implemented NewPostgresConnection function.

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/olivo4ka37/WB_L0/repository"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
@@ -9,6 +10,18 @@ func main() {
 	err := initConfig()
 	if err != nil {
 		logrus.Fatalf("error initializing configs:%s", err.Error())
+	}
+
+	database, err := repository.NewPostgresConnection(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: viper.GetString("db.password"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		logrus.Fatalf("failed to connect database:%s", err.Error())
 	}
 }
 
