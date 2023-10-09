e6d804d246b0809cc5c97374b312a6b1006cdeb8
Egor Kostylev <46248025+olivo4ka37@users.noreply.github.com>
2023-10-09 04:48:21 +0300

    Added func initConfig for initializing config data in func main

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
 
+func main() {
+	err := initConfig()
+	if err != nil {
+		logrus.Fatalf("error initializing configs:%s", err.Error())
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
