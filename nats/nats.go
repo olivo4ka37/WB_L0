d1850decc0f9f2c69571a69e60e4f9c43319d0e8
Egor Kostylev <46248025+olivo4ka37@users.noreply.github.com>
2023-10-16 05:18:33 +0300

    Deleted nahui nats, it was mistake

--- nats/nats.go
+++
@@ -1,7 +0,0 @@
-package nats
-
-import "github.com/nats-io/stan.go"
-
-func NatsConnect(clusterID, clientID string) (stan.Conn, error) {
-	return stan.Connect(clusterID, clientID, stan.NatsURL("0.0.0.0:4222"))
-}
